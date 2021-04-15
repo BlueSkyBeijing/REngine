d4a4000e60ad0cc682a66f9e4b1c9ed4597f176d
BlueSkyBeijing <chenjunlong@kingsoft.com>
2021-04-15 20:18:13 +0800

    fixed secular lighting error

--- Engine/Shader/ForwardShadingPS.hlsl
+++ Engine/Shader/ForwardShadingPS.hlsl
@@ -8,8 +8,9 @@ float4 PSMain(VertexOut PIn) : SV_Target
     float4 outColor;
     
     const float4 diffuseColor = DiffuseMap.Sample(DiffuseSamplerState, PIn.TexCoord);
+    const float3 viewDir = normalize(gCameraPos - PIn.Pos.xyz);
     const float lightIntensity = 0.8f;
-    float3 lighting = BlinnPhong(PIn.Normal, gDirectionalLightDir, gDirectionalLightColor, lightIntensity, gCameraDir, diffuseColor.rgb);
+    float3 lighting = BlinnPhong(PIn.Normal, gDirectionalLightDir, gDirectionalLightColor, lightIntensity, viewDir, diffuseColor.rgb);
 
     outColor.rgb = lighting;
     outColor.a = 1.0f;
