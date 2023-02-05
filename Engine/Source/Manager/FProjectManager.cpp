75f3ed0fc04c1187f59c068644c55387f6c11237
chenjunlong <chenjunlong@kingsoft.com>
2023-02-05 11:32:45 +0800

    add project manager frame

---
+++ Engine/Source/Manager/FProjectManager.cpp
@@ -0,0 +1,27 @@
+﻿#include "PrecompiledHeader.h"
+
+#include "FProjectManager.h"
+#include "TSingleton.h"
+#include "IModuleInterface.h"
+
+FProjectManager::FProjectManager()
+{
+}
+
+FProjectManager::~FProjectManager()
+{
+}
+
+void FProjectManager::Init()
+{
+
+}
+
+void FProjectManager::UnInit()
+{
+}
+
+bool  FProjectManager::LoadProjectFile(const FString& ProjectFile)
+{
+	return true;
+}
---
+++ Engine/Source/Manager/FProjectManager.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "Prerequisite.h"
+
+#include "TSingleton.h"
+
+
+class FProjectManager
+{
+    friend class TSingleton<FProjectManager>;
+
+public:
+    void Init();
+    void UnInit();
+
+    bool LoadProjectFile(const FString& ProjectFile);
+
+private:
+    FProjectManager();
+    virtual ~FProjectManager();
+
+private:
+};
